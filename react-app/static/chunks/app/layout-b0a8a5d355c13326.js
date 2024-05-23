27c8ab923b56fdd7ef2074c68507a2399084b777
James Wilson <jwi3581@wgu.edu>
2024-05-23 16:13:58 -0400

    Add React project to GitHub Pages website

---
+++ react-app/static/chunks/app/layout-b0a8a5d355c13326.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2005:function(n,e,u){Promise.resolve().then(u.t.bind(u,1287,23))},1287:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=2005)}),_N_E=n.O()}]);
